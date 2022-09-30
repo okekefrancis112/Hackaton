6b0d6fc345ff67faf5c79f3ec13454cc6d217923
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-09-30 17:30:31 +0100

    update scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,34 @@
 import { ethers } from "hardhat";
 
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+//   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+//   const lockedAmount = ethers.utils.parseEther("1");
+
+//   const Lock = await ethers.getContractFactory("Lock");
+//   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+//   await lock.deployed();
+
+//   console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+// }
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  // const lockedAmount = ethers.utils.parseEther("1");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const address = "0x23d5C0bAdf63ff6422B5B9310211d9BcE147e720";
+  const Lock = await ethers.getContractFactory("HodlToken");
+  const lock = await Lock.deploy();
 
-  await lock.deployed();
+  const  res = await lock.deployed();
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log("successfully deployed >>>>>>>", res.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
